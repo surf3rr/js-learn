fa703433cfa62887565248956f06a1f61f808697
surf3rr <utkarshsingh2321@gmail.com>
2026-08-18 18:34:14 +0530

    learning js

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 2
+
+let accountEmail = "utkarsh@gmail.com";
+
+var accountPass = "123abc";
+
+accountCity = "Lucknow";
+
+let accState;
+
+console.log(accountId)
+
+console.table([accountCity,accountPass,accountEmail,accState])
+
+// prefer not to use var because issue in block scope and fun size
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,8 @@
+//alert(3+2) we are using node js not browser
+
+let name = "Utkarsh"
+let age =18
+let isLogged = false
+
+// null is a standalone value
+// undefined means value is not assigned yet

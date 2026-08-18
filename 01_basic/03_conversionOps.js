f7fcd659f0eedd67ab5834fc34a0ed773698a2d2
surf3rr <utkarshsingh2321@gmail.com>
2026-08-18 19:02:10 +0530

    conversion of datatypes

---
+++ 01_basic/03_conversionOps.js
@@ -0,0 +1,14 @@
+score = "33"
+
+console.log(typeof(score))
+
+let changingScore = Number(score)
+
+console.log(typeof changingScore)
+
+
+//"33" => number
+//33abc => NaN
+//undefined => Nan
+//null = 
+//true => 1

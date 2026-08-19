965ea265cf507145c6c6f1db33952abef13195c3
surf3rr <utkarshsingh2321@gmail.com>
2026-08-19 18:31:37 +0530

    summary datatypes

---
+++ 01_basic/datatypesSummary.js
@@ -0,0 +1,20 @@
+// Primitive 
+
+// 7 types : string,number, big int, bool,  undefined, null
+
+//js is a dynamically typed language
+
+// Refrence Type
+
+// array, object, function
+
+const name = ["utkarsh","ashar","garvit"]
+
+let myObj = {
+    name: "utkarsh",
+    age: 18
+}
+
+const myFun = function(){
+    console.log("hello world")
+}

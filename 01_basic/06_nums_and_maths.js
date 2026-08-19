69450651c7d821a97ec5bd890584ba57ea339a9f
surf3rr <utkarshsingh2321@gmail.com>
2026-08-19 21:07:03 +0530

    maths and numbers

---
+++ 01_basic/06_nums_and_maths.js
@@ -0,0 +1,18 @@
+const balance = new Number('4000')
+console.log(balance)
+
+console.log(balance.toString())
+
+const num = new Number(300.59)
+
+console.log(num.toPrecision(3));
+
+const hundered = 10000000
+console.log(hundered.toLocaleString('en-IN'))
+
+console.log((Math.random()*10) +1)
+
+const min = 10
+const max = 50
+
+console.log(Math.floor(Math.random()*(max - min + 1)+ min));
